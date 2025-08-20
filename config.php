1a284478babe7a4c22b0d3527bb08427f5d34703
Toxicnonoal <104231519+Toxicnonoal@users.noreply.github.com>
2025-08-21 01:39:31 +0330

    config.php

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+// 🔹 توکن ربات
+$BOT_TOKEN = "YOUR_BOT_TOKEN";
+
+// 🔹 آیدی عددی ادمین
+$ADMIN_ID = 123456789;
+
+// 🔹 اطلاعات دیتابیس
+$DB_NAME = "your_db_name";
+$DB_USER = "your_db_user";
+$DB_PASS = "your_db_pass";
+$DB_HOST = "localhost";
+
+// 🔹 دامنه
+$DOMAIN = "yourdomain.com";
+
+// 🔹 مرچنت آی‌دی زرین‌پال
+$ZARINPAL_MERCHANT = "your_zarinpal_merchant_id";
+?>
